2fa146bebdedbffe09d96d37ab4aa17cefb351b0
gnbat0606 <batuk20@yahoo.com>
2024-07-26 12:38:00 +0800

    7-26 classwork

---
+++ DOM/TODO/todo.js
@@ -0,0 +1,36 @@
+const arr = ["Todo", "In progress", "Stuck", "Done"];
+const boards = document.getElementById("boards");
+console.log(boards);
+arr.forEach((names) => {
+  boards.innerHTML += `<div class="board">
+          <div class="board-header"> ${names} <span class="count">0</span></div>
+          <div class="cards" id="todo">
+            <div class="card">
+              <div class="done"><i class="fa-solid fa-check"></i></div>
+              <div class="detail">
+                <h4>Todo</h4>
+                <p>This is a Todo card</p>
+                <div class="priority">High</div>
+              </div>
+              <div class="actions">
+                <div class="done"><i class="fa-solid fa-xmark"></i></div>
+                <div class="done">
+                  <i class="fa-solid fa-pen-to-square"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="addBtn" class="add-btn">
+            <i class="fa-solid fa-plus"></i>
+            <div>Add card</div>
+          </div>
+        </div>`;
+});
+
+const addBtn = document.getElementById("addBtn");
+const modalContainer = document.getElementById("modalContainer");
+
+addBtn.addEventListener("click", () => {
+  console.log(addBtn);
+  modalContainer.style.display = "flex";
+});
