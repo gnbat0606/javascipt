717946fa79ef3f865da547765893dd74f0f1efc7
gnbat0606 <batuk20@yahoo.com>
2024-07-29 13:17:55 +0800

    7-29

--- DOM/TODO/todo.js
+++ DOM/TODO/todo.js
@@ -1,24 +1,17 @@
 const arr = ["Todo", "In progress", "Stuck", "Done"];
 const boards = document.getElementById("boards");
+
+const inputValues = {
+  title: "",
+  desc: "",
+  status: "",
+  priority: "",
+};
+
 arr.forEach((names) => {
-  boards.innerHTML += `<div class="board">
+  boards.innerHTML += `<div class="board" id="board">
           <div class="board-header"> ${names} <span class="count">0</span></div>
-          <div class="cards" id="todo">
-            <div class="card">
-              <div class="done"><i class="fa-solid fa-check"></i></div>
-              <div class="detail">
-                <h4>Todo</h4>
-                <p>This is a Todo card</p>
-                <div class="priority">High</div>
-              </div>
-              <div class="actions">
-                <div class="done"><i class="fa-solid fa-xmark"></i></div>
-                <div class="done">
-                  <i class="fa-solid fa-pen-to-square"></i>
-                </div>
-              </div>
-            </div>
-          </div>
+          <div class="cards"></div>
           <div id="addBtn" class="add-btn">
             <i class="fa-solid fa-plus"></i>
             <div>Add card</div>
@@ -26,19 +19,72 @@ arr.forEach((names) => {
         </div>`;
 });
 
-const addBtn = document.getElementById("addBtn");
+const cardCreator = () => {
+  return `<div class="card">
+    <div class="done">
+      <i class="fa-solid fa-check"></i>
+    </div>
+    <div class="detail">
+      <h4 class="h4">${inputValues.status}</h4>
+      <p class="desText">${inputValues.title}</p>
+      <div class="priority">${inputValues.desc}</div>
+    </div>
+    <div class="actions">
+      <div class="done">
+        <i class="fa-solid fa-xmark"></i>
+      </div>
+      <div class="done">
+        <i class="fa-solid fa-pen-to-square"></i>
+      </div>
+    </div>
+  </div>`;
+};
+
+//+tsonh nemeh +card nemeh
+const addBtns = document.querySelectorAll(".add-btn");
 const modalContainer = document.getElementById("modalContainer");
-
-console.log(boards);
-
-console.log(boards.innerHTML);
-
-addBtn.addEventListener("click", () => {
-  console.log(addBtn);
-  modalContainer.style.display = "flex";
+addBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    modalContainer.style.display = "flex";
+  });
 });
 
-const away = document.getElementById("away");
-away.addEventListener("click", () => {
+//-tsonhiig gargah ghde submit button uyd
+//+++ value log
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const stat = document.getElementById("stat");
+const priority = document.getElementById("priority");
+////////////////////////
+const cards = document.querySelectorAll(".cards");
+// const h4 = document.querySelectorAll(".h4");
+// const desText = document.querySelectorAll(".desText");
+// const priorityText = document.querySelectorAll(".priority");
+////////////////////////
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
   modalContainer.style.display = "none";
+
+  inputValues.title = title.value;
+  inputValues.desc = description.value;
+  inputValues.status = stat.value;
+  inputValues.priority = priority.value;
+
+  const craetedCard = cardCreator();
+
+  // cards.forEach(())
+
+  if (stat.value == "todo") {
+    cards[0].innerHTML += craetedCard;
+  }
 });
+
+//+tsonhiig gargah
+window.addEventListener("click", (event) => {
+  if (event.target == modalContainer) {
+    modalContainer.style.display = "none";
+  }
+});
+
+//
