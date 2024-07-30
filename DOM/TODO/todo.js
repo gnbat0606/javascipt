66b119ec20c038cdacf8a04e47c5d570db8bc7aa
gnbat0606 <batuk20@yahoo.com>
2024-07-30 23:18:12 +0800

    gertee 7-30

--- DOM/TODO/todo.js
+++ DOM/TODO/todo.js
@@ -1,11 +1,13 @@
 const arr = ["Todo", "In progress", "Stuck", "Done"];
 const boards = document.getElementById("boards");
+const deleteCard = document.getElementById("deleteCard");
 
 const inputValues = {
   title: "",
   desc: "",
   status: "",
   priority: "",
+  id: "",
 };
 
 arr.forEach((names) => {
@@ -25,15 +27,15 @@ const cardCreator = () => {
       <i class="fa-solid fa-check"></i>
     </div>
     <div class="detail">
-      <h4 class="h4">${inputValues.status}</h4>
-      <p class="desText">${inputValues.title}</p>
-      <div class="priority">${inputValues.desc}</div>
+      <h4 class="h4">${inputValues.title}</h4>
+      <p class="desText">${inputValues.desc}</p>
+      <div class="priority">${inputValues.priority}</div>
     </div>
     <div class="actions">
       <div class="done">
         <i class="fa-solid fa-xmark"></i>
       </div>
-      <div class="done">
+      <div class="done" id="deleteCard">
         <i class="fa-solid fa-pen-to-square"></i>
       </div>
     </div>
@@ -43,8 +45,8 @@ const cardCreator = () => {
 //+tsonh nemeh +card nemeh
 const addBtns = document.querySelectorAll(".add-btn");
 const modalContainer = document.getElementById("modalContainer");
-addBtns.forEach((button) => {
-  button.addEventListener("click", () => {
+addBtns.forEach((halo) => {
+  halo.addEventListener("click", () => {
     modalContainer.style.display = "flex";
   });
 });
@@ -55,13 +57,9 @@ const title = document.getElementById("title");
 const description = document.getElementById("description");
 const stat = document.getElementById("stat");
 const priority = document.getElementById("priority");
-////////////////////////
 const cards = document.querySelectorAll(".cards");
-// const h4 = document.querySelectorAll(".h4");
-// const desText = document.querySelectorAll(".desText");
-// const priorityText = document.querySelectorAll(".priority");
-////////////////////////
 const submitButton = document.getElementById("submitButton");
+////////////////////////
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
   modalContainer.style.display = "none";
@@ -71,16 +69,30 @@ submitButton.addEventListener("click", (event) => {
   inputValues.status = stat.value;
   inputValues.priority = priority.value;
 
-  const craetedCard = cardCreator();
+  const createdCard = cardCreator();
 
-  // cards.forEach(())
+  if (title.value == "") {
+    alert("title оруулна уу");
+    throw new Error("title hooson bn");
+  }
+  if (description.value == "") {
+    alert("description оруулна уу");
+    throw new Error("title hooson bn");
+  }
 
   if (stat.value == "todo") {
-    cards[0].innerHTML += craetedCard;
+    cards[0].innerHTML += createdCard;
+  } else if (stat.value == "inprogress") {
+    cards[1].innerHTML += createdCard;
+  } else if (stat.value == "stuck") {
+    cards[2].innerHTML += createdCard;
+  } else if (stat.value == "done") {
+    cards[3].innerHTML += createdCard;
   }
 });
 
 //+tsonhiig gargah
+
 window.addEventListener("click", (event) => {
   if (event.target == modalContainer) {
     modalContainer.style.display = "none";
