a978bec1ba506969e50627cecb1c2a0b020990b9
gnbat0606 <batuk20@yahoo.com>
2024-08-05 12:46:42 +0800

    8-4

---
+++ array/8-5.js
@@ -0,0 +1,11 @@
+const array = [1, 2, 3, 4, 5];
+const array1 = [7, 2, 3, 4, 5];
+const array2 = [5, 4, 3, 2, 1];
+const array3 = [3, 4, 5, 6, 7];
+
+const func = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+func(array1);
